12823fc2548ced50b6706abf9886881719707bd1
Justin Bruinsma <justinabruinsma@gmail.com>
2025-10-15 23:23:35 -0400

    Refactor App.vue to use HomePage component and add HomePage.vue with initial content

---
+++ frontend/src/views/HomePage.vue
@@ -0,0 +1,19 @@
+<!--// ALL HTML GOES inside  < template > < /template>/-->
+<template>
+  <div class="main-container">
+    <h1>THIS IS THE HOME PAGE</h1>
+    <p>LOOK AT FILE FOR INSTRUCTIONS</p>
+    <p> /frotend -> /src -> /views -> HomePage.vue </p>
+  </div>
+</template>
+
+<script setup>
+// ALL JAVASCRIPT LOGIC GOES HERE
+</script>
+
+<style scoped>
+// ALL CSS STYLES GO HERE
+
+.main-container { max-width: 800px; margin: 40px auto; padding: 20px; text-align: center; font-family: 'Avenir', Helvetica, Arial, sans-serif; color: #2c3e50; }
+h1 { color: #42b983; }
+</style>
